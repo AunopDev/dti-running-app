f74e8abb093f6f0196cdf251067efd22c97cdfca
AunopDev <s6452410004@sau.ac.th>
2025-03-21 13:29:01 +0700

    Fix Route Again

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // กำหนด base URL เป็น root
   server:{
     port: 5566,
+    host: true  // อนุญาตให้เข้าถึงจากภายนอกได้
   }
 })
